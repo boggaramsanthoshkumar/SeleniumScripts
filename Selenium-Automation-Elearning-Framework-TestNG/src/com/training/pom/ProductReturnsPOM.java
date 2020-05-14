146328a1fe3b7bd931651a94c3f02ed9b5f8af62
SanthoshKumarBoggara <SanthoshKumarBoggara@DESKTOP-RU0JSMC>
2020-05-14 10:34:10 +0530

    Medium Test Cases - RTTC034, RTTC035, RTTC036 - Commit(2)

--- Selenium-Automation-Elearning-Framework-TestNG/src/com/training/pom/ProductReturnsPOM.java
+++ Selenium-Automation-Elearning-Framework-TestNG/src/com/training/pom/ProductReturnsPOM.java
@@ -22,7 +22,8 @@ public class ProductReturnsPOM {
 	@FindBy(xpath="//div[@class='col-sm-10']//div[1]//label[1]")
 	private WebElement RRDeadOnArrival;
 	
-	@FindBy(xpath="//body[@class='account-return-add-1161 tb_width_1200 tb_lang_ltr tb_page_account_return_add is_logged head_glob_intro_glob_cont_glob_foot_glob_ar_head_glob_ar_intro__def_ar_cont__def_ar_col_l_glob_ar_foot_glob_en-gb_733d2 no_touch']/div[@id='wrapper']/section[@id='content']/div[@class='row tb_separate_columns tb_ip_50']/div[@class='main col col-xs-12 col-sm-fill col-md-fill']/div[@class='row_8rjbB row-wrap lazyloaded']/div[@class='row tb_gut_xs_30 tb_gut_sm_30 tb_gut_md_50 tb_gut_lg_50']/div[@class='col_hJRg2 col col-xs-12 col-sm-12 col-md-12 col-lg-12 col-align-default col-valign-top tb_pt_0 tb_pr_0 tb_pb_0 tb_pl_0']/div[@id='System_s3bhXNDb']/form[@id='return_request_form']/fieldset/div[@class='form-group required']/div[@class='col-sm-10']/label[1]")
+	//@FindBy(xpath="//body[@class='account-return-add-1161 tb_width_1200 tb_lang_ltr tb_page_account_return_add is_logged head_glob_intro_glob_cont_glob_foot_glob_ar_head_glob_ar_intro__def_ar_cont__def_ar_col_l_glob_ar_foot_glob_en-gb_733d2 no_touch']/div[@id='wrapper']/section[@id='content']/div[@class='row tb_separate_columns tb_ip_50']/div[@class='main col col-xs-12 col-sm-fill col-md-fill']/div[@class='row_8rjbB row-wrap lazyloaded']/div[@class='row tb_gut_xs_30 tb_gut_sm_30 tb_gut_md_50 tb_gut_lg_50']/div[@class='col_hJRg2 col col-xs-12 col-sm-12 col-md-12 col-lg-12 col-align-default col-valign-top tb_pt_0 tb_pr_0 tb_pb_0 tb_pl_0']/div[@id='System_s3bhXNDb']/form[@id='return_request_form']/fieldset/div[@class='form-group required']/div[@class='col-sm-10']/label[1]")
+	@FindBy(xpath="//label[@class='radio-inline']//input[@value='1']")
 	private WebElement IsProductOpenedYes;
 	
 	@FindBy(xpath="//input[@type='radio'][@name='return_reason_id'][@value='1']")
